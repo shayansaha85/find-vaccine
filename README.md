6b5799468234308aabd6d7e6cb784655800aadb8
Shayan Saha <shayan851997@gmail.com>
2021-05-30 22:50:59 +0530

    edited for showing multiple results if available

---
+++ README.md
@@ -0,0 +1,5 @@
+# Find-Vaccine
+
+This website can help you to find vaccine center availibility by giving your PIN and date
+
+Link : https://shayansaha85.github.io/find-vaccine
--- rest.js
+++ rest.js
@@ -1,12 +1,12 @@
 let button = document.querySelector("#findVac");
 
-
+let outputString = "";
 
 button.addEventListener('click', ()=> {
 let pin = document.querySelector("#pin").value;
 let date = document.querySelector("#date").value;
 let url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pin + "&date= " + date;
-
+https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=799001&date=31-05-2021"
 
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
@@ -26,30 +26,30 @@ console.log(jsonOP);
 let n = jsonOP["sessions"].length
 console.log(n)
 let count=0;
+
 for(var i=0;i<n;i++) {
-    if(jsonOP["sessions"][i]["available_capacity"] != 0){
-    document.querySelector("#name").innerHTML = "<b>Name : </b>" + jsonOP["sessions"][i]["name"]
-    document.querySelector("#address").innerHTML ="<b>Address : </b>" + jsonOP["sessions"][i]["address"]
-    document.querySelector("#dose1").innerHTML ="<b>DOSE 1 : </b>" + jsonOP["sessions"][i]["available_capacity_dose1"]
-    document.querySelector("#dose2").innerHTML ="<b>DOSE 2 : </b>" + jsonOP["sessions"][i]["available_capacity_dose2"]
-    document.querySelector("#availibility").innerHTML ="<b>Available capacity : </b>" + jsonOP["sessions"][i]["available_capacity"]
-    document.querySelector("#minimumAge").innerHTML = "<b>Mininum age : </b>" + jsonOP["sessions"][i]["min_age_limit"]
-    document.querySelector("#vaccine").innerHTML ="<b>Vaccine : </b>" + jsonOP["sessions"][i]["vaccine"]
-    document.querySelector("#notAv").innerHTML = ""
-    document.querySelector("#lineBreak").innerHTML = "<br><hr>-<br>"
-    count++;
+    if(jsonOP["sessions"][i]["available_capacity"] != 0) {
+        outputString += "<b>Name of center : </b>" + jsonOP["sessions"][i]["name"]+"<br>"+
+        "<b>Address : </b>" + jsonOP["sessions"][i]["address"]+"<br>"+
+        "<b>DOSE 1 capacity : </b>" + jsonOP["sessions"][i]["available_capacity_dose1"]+"<br>"+
+        "<b>DOSE 2 capacity : </b>" + jsonOP["sessions"][i]["available_capacity_dose2"]+"<br>"+
+        "<b>Available capacity : </b>" + jsonOP["sessions"][i]["available_capacity"]+"<br>"+
+        "<b>Mininum age : </b>" + jsonOP["sessions"][i]["min_age_limit"]+"<br>"+
+        "<b>Vaccine : </b>" + jsonOP["sessions"][i]["vaccine"]+"<br>"+
+        "<br><hr><br>"
+        count++
     }
+   
 }
-if(count==0) {
-    document.querySelector("#name").innerHTML = ""
-    document.querySelector("#address").innerHTML =""
-    document.querySelector("#dose1").innerHTML =""
-    document.querySelector("#dose2").innerHTML =""
-    document.querySelector("#availibility").innerHTML =""
-    document.querySelector("#minimumAge").innerHTML = ""
-    document.querySelector("#vaccine").innerHTML =""
-    document.querySelector("#notAv").innerHTML = "<strong>Vaccine not available</strong>"
-}
+    if(count!=0) {
+        document.querySelector("#data").innerHTML = outputString
+        document.querySelector("#notAv").innerHTML = ""
+    } else {
+        document.querySelector("#data").innerHTML = ""
+        document.querySelector("#notAv").innerHTML = "Vaccine not available"
+    }
+    
+
 
 })
 .catch(error => console.log('error', error));
