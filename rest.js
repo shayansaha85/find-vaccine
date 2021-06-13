5476c94b2204547a18c89ed86aeba65174ba8065
Shayan Saha <shayan851997@gmail.com>
2021-06-13 10:22:05 +0530

    added age group option

--- rest.js
+++ rest.js
@@ -18,6 +18,7 @@ date = date.substring(0, date.length-1)
 
 let url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pin + "&date= " + date;
 
+let ageGroup = parseInt(document.getElementById("sel1").value)
 
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
@@ -39,13 +40,13 @@ console.log(n)
 let count=0;
 
 for(var i=0;i<n;i++) {
-    if(jsonOP["sessions"][i]["available_capacity"]!=0) {
+    if(jsonOP["sessions"][i]["available_capacity"]!=0 && jsonOP["sessions"][i]["min_age_limit"] == ageGroup) {
         outputString += "<b>Name of centre : </b>" + jsonOP["sessions"][i]["name"]+"<br>"+
         "<b>Address : </b>" + jsonOP["sessions"][i]["address"]+"<br>"+
         "<b>DOSE 1 capacity : </b>" + jsonOP["sessions"][i]["available_capacity_dose1"]+"<br>"+
         "<b>DOSE 2 capacity : </b>" + jsonOP["sessions"][i]["available_capacity_dose2"]+"<br>"+
-        "<b>Available capacity : </b>" + jsonOP["sessions"][i]["available_capacity"]+"<br>"+
-        "<b>Mininum age : </b>" + jsonOP["sessions"][i]["min_age_limit"]+"<br>"+
+        "<b>Total capacity : </b>" + jsonOP["sessions"][i]["available_capacity"]+"<br>"+
+        "<b>Age Group : </b>" + showAge(jsonOP["sessions"][i]["min_age_limit"])+"<br>"+
         "<b>Vaccine : </b>" + jsonOP["sessions"][i]["vaccine"]+"<br>"+
         "<br><hr><br>"
         count++
@@ -57,9 +58,18 @@ for(var i=0;i<n;i++) {
         document.querySelector("#notAv").innerHTML = ""
     } else {
         document.querySelector("#data").innerHTML = ""
-        alert("Vaccination centres not availabe")
-        document.querySelector("#notAv").innerHTML = "No vaccination centres are available"
+        alert("Vaccine not availabe")
+        document.querySelector("#notAv").innerHTML = "No vaccination centres are available for this age group"
     }
 })
 .catch(error => console.log('error', error));
 })
+
+
+function showAge(age) {
+    if(age == 18) {
+        return "18-44"
+    } else {
+        return "45 and above"
+    }
+}
